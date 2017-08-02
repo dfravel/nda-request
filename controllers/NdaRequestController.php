4e3e2bd880de9d7d384289de6579c8e6c416bd92
Dave Fravel <dave@bluegear.io>
2017-08-02 15:56:51 -0400

    Completed development of version 1

--- controllers/NdaRequestController.php
+++ controllers/NdaRequestController.php
@@ -17,21 +17,46 @@ namespace Craft;
 class NdaRequestController extends BaseController
 {
 
-    /**
-     * @var    bool|array Allows anonymous access to this controller's actions.
-     * @access protected
-     */
-    protected $allowAnonymous = array('actionIndex',
-        );
-
-    /**
-     * Handle a request going to our plugin's index action URL, e.g.: actions/ndaRequest
-     */
+    protected $allowAnonymous = true;
     public function actionIndex()
     {
+        $this->requirePostRequest();
+        $password = craft()->request->getPost('access-interview');
+
+        if ($this->_validPassword($password)) {
+            // not doing a cookie for this one, but leaving this code here as an example
+            // $expires = time() + (60*60*24*7); // One week
+            // setcookie(md5('exariNDA'), 1, $expires, '/');
+
+            $this->redirect($this->_getRedirect());
+        } else {
+            craft()->urlManager->setRouteVariables(array(
+                'error' => 'Oops! That Password doesn\'t seem to match'
+            ));
+        }
     }
 
-    public function actionValidate()
+    private function _getRedirect()
     {
+        // 2767 represents the global data set that we're using for the NDA Interview
+        $globalSet = craft()->globals->getSetById(2768);
+
+        // we have a field in that to define the redirect on success
+        $redirect = $globalSet->websiteLink;
+
+        return $redirect;
+    }
+
+
+    private function _validPassword($pwEntered)
+    {
+        // 2767 represents the global data set that we're using for the NDA Interview
+        $globalSet = craft()->globals->getSetById(2768);
+
+        // Get target password
+        $pwValid = $globalSet->passwordProtection;
+
+        // Return whether password matches
+        return ($pwEntered === $pwValid);
     }
 }
